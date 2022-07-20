1df5c41823f53d82b6a40f3ce9dfa49d1033da8d
Alejandro Abad <alejandro.abad@hefame.es>
2022-07-20 23:26:31 +0200

    Ahora se persiste el estado del usuario

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import pantallaReducer from './pantallaSlice';
 import apiReducer from './api/apiSlice';
 import catalogoReducer from './api/catalogoSlice';
 import carritoReducer from './api/carritoSlice';
@@ -7,7 +8,29 @@ import valesReducer from './api/valesSlice';
 import albaranPdfReducer from './api/albaranPdfSlice';
 import nominaPdfReducer from './api/nominaPdfSlice';
 import accesosReducer from './api/accesosSlice';
-import pantallaReducer from './pantallaSlice';
+import ticketsReducer from './api/ticketsSlice';
+
+
+const loadState = () => {
+//	localStorage.removeItem('state');
+	try {
+		const estadoSerializado = localStorage.getItem('state');
+		if (estadoSerializado === null) return undefined;
+		return JSON.parse(estadoSerializado);
+	} catch (e) {
+		console.log(e);
+		return undefined;
+	}
+}
+
+const saveState = (state) => {
+	try {
+		const estadoSerializado = JSON.stringify(state);
+		localStorage.setItem('state', estadoSerializado);
+	} catch (e) {
+		console.log(e);
+	}
+}
 
 export const store = configureStore({
 	reducer: {
@@ -18,6 +41,18 @@ export const store = configureStore({
 		albaranPdf: albaranPdfReducer,
 		nominaPdf: nominaPdfReducer,
 		pantalla: pantallaReducer,
-		accesos: accesosReducer
+		accesos: accesosReducer,
+		tickets: ticketsReducer
 	},
+	preloadedState: loadState()
 });
+
+store.subscribe(() => {
+	let state = store.getState();
+	saveState({
+		api: state.api,
+		carrito: {
+			materiales: state.carrito.materiales
+		}
+	})
+})
